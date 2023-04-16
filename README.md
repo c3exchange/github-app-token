6708ca0dfeafd1b3d60b6b2f4ede35c4e1a08763
Mauro Leggieri <mxmauro@gmail.com>
2023-04-16 09:46:26 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# github-app-token
-A tool to request a new access token from an installed GitHub Application
+# GitHub-App-Token
+
+A tool to request a new access token from an installed GitHub Application.
+
+## Prerequisites
+
+Create a custom GitHub Application by going to your account or organization settings -> `Developer Settings` -> `GitHub Apps` and
+click on the `New GitHub App` button
+
+1. Enter the application name, homepage and other required basic information. Disable WebHooks if not needed.
+2. Configure the permissions this application will have and where can be installed.
+3. Take note of the generate `Application ID`.
+4. Once the application has been created install it on your organization or personal account. If you omit this step, you can install
+later by editing the application on the `GitHub Apps` settings page.
+
+Generate a private key:
+
+1. Go back to your account or organization settings -> `Developer Settings` -> `GitHub Apps` and click on the `Edit` button of your
+newly created application.
+2. Scroll down in the General settings and look for the `Generate a private key` button.
+3. Save the newly private key in a **SECURE** location like a GitHub secret.
+
+## Usage
+
+Make sure you have [NodeJS](https://nodejs.org/en/download) installed as well as [`npm`](https://www.npmjs.com/package/npm) and
+[`npx`](https://www.npmjs.com/package/npx) modules.
+
+Run `npx github:c3exchange/github-app-token {parameters}`
+
+Where `parameters` can be:
+
+* `--pk {private-key-file}` - A file containing the generated RSA-256 private key. If `private-key-file` is equal to `stdin`, the
+script will read the private key from the standard input.
+* `--app-id {application-id}` - Specifies the application ID.
+* `--perm {list-of-permissions}` - A list of requested permissions separated by comma. See below for details.
+* `--ua {user-agent}` - An  user agent to use in web requests. Defaults to: `GitHubAppToken-Retriever/1.0`.
+
+### Permissions
+
+Each permission consist in strings with the following format: `access:type`
+
+Type can be: `contents`, `packages`, `secrets`, etc. See GitHub documentation for a complete list of access types.
+
+Access is optional and must be separated from the type with a `:` (dash). If specified, it can be `read`, `write` or `admin`.
+
+Some examples:
+
+* `write:contents` allows write access to repository contents.
+* `packages` allows read access to packages.
+* `read:secrets` allows read access to secrets in a GitHub action.
+
+# License
+
+Apache 2.0
---
+++ bin/cli.js
@@ -0,0 +1,247 @@
+#! /usr/bin/env node
+
+/**
+ * Copyright (c) 2023, C3.io
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+const crypto = require("crypto");
+const https = require("https");
+const process = require("process");
+const fs = require("fs");
+
+// -----------------------------------------------------------------------------
+
+main().catch((err) => {
+	console.error("Error: " + err.message);
+	process.exit(1);
+});
+
+async function main() {
+	const opts = parseArguments();
+
+	// load private key from file
+	let privateKey;
+	if (opts.privateKeyFile != 'stdin') {
+		privateKey = fs.readFileSync(opts.privateKeyFile, "utf8");
+	}
+	else {
+		privateKey = await readStdIn();
+	}
+
+	// create payload for JWT
+	const header = toBase64Url(Buffer.from(JSON.stringify({
+		"alg": "RS256",
+		"typ": "JWT"
+	}), "binary"));
+
+	const currTime = Math.floor(Date.now() / 1000);
+	const payload = toBase64Url(Buffer.from(JSON.stringify({
+		"iat": currTime,
+		"exp": currTime + 540,
+		"iss": opts.applicationId.toString()
+	}), "utf8"));
+
+	// sign JWT
+	const signer = crypto.createSign("RSA-SHA256");
+	signer.update(header + "." + payload)
+	const jwtToken = header + "." + payload + "." + toBase64Url(signer.sign(privateKey));
+
+	// build header for requests
+	const reqHdrs = {
+		"Accept": "application/vnd.github+json",
+		"Authorization": "Bearer " + jwtToken,
+		"User-Agent": (opts.userAgent ? opts.userAgent : "GitHubAppToken-Retriever/1.0"),
+		"X-GitHub-Api-Version": "2022-11-28"
+	};
+
+	// retrieve application's installation id
+	let res = await webRequest("https://api.github.com/app/installations", {
+		headers: reqHdrs
+	});
+	if (res.statusCode !== 200) {
+		console.log(res.data);
+		throw new Error("Retrieval of installed applications failed with status " + res.statusCode.toString());
+	}
+
+	let installationId = 0;
+	for (const app of JSON.parse(res.data)) {
+		if (app.app_id === opts.applicationId) {
+			installationId = app.id;
+			break;
+		}
+	}
+	if (installationId == 0) {
+		throw new Error("Unable to locate installation ID");
+	}
+
+	// request an access token for the application instance
+	res = await webRequest("https://api.github.com/app/installations/" + installationId.toString() + "/access_tokens", {
+		headers: reqHdrs,
+		method: "POST",
+		body: JSON.stringify({
+			permissions: opts.permissions
+		})
+	});
+	if (res.statusCode !== 201) {
+		throw new Error("Retrieval of application access token failed with status " + res.statusCode.toString());
+	}
+	//parse response
+	res = JSON.parse(res.data);
+
+	//print token
+	process.stdout.write(res.token);
+}
+
+function parseArguments() {
+	const permissionsRegex = /[a-z\-]+/u;
+	const opts = {};
+
+	const args = process.argv.slice(2);
+	if (args.length == 0) {
+		throw new Error(
+			"Missing arguments. Use: npx github-app-token --pk {private-key-file} --app-id application-id " +
+			"--perm {list-of-permissions} [--ua {user-agent}]"
+		);
+	}
+
+	//parse command line arguments
+	for (let idx = 0; idx < args.length; idx += 1) {
+		if (args[idx] == "--pk") {
+			if (opts.privateKeyFile) {
+				throw new Error("Private key file already specified.");
+			}
+			idx += 1 
+			if (idx >= args.length || args[idx].length == 0) {
+				throw new Error("Missing argument for '--pk' parameter.");
+			}
+			opts.privateKeyFile = args[idx];
+		}
+		else if (args[idx] == "--app-id") {
+			if (opts.applicationId) {
+				throw new Error("Application ID already specified.");
+			}
+			idx += 1;
+			if (idx >= args.length || args[idx].length == 0) {
+				throw new Error("Missing argument for '--app-id' parameter.");
+			}
+			opts.applicationId = parseInt(args[idx], 10);
+			if (isNaN(opts.applicationId) || opts.applicationId < 1) {
+				throw new Error("Invalid application ID.");
+			}
+		}
+		else if (args[idx] == "--perm") {
+			if (opts.permissions) {
+				throw new Error("Access permissions already specified.");
+			}
+			idx += 1;
+			if (idx >= args.length || args[idx].length == 0) {
+				throw new Error("Missing argument for '--perm' parameter.");
+			}
+			opts.permissions = {};
+			for (let perm of args[idx].split(",")) {
+				let access = "read";
+				if (perm.startsWith("read:")) {
+					perm = perm.substring(5);
+				}
+				else if (perm.startsWith("write:")) {
+					access = "write";
+					perm = perm.substring(6);
+				}
+				else if (perm.startsWith("admin:")) {
+					access = "admin";
+					perm = perm.substring(6);
+				}
+				if (!permissionsRegex.test(perm)) {
+					throw new Error("Invalid permission name.");
+				}
+				opts.permissions[perm] = access;
+			}
+		}
+		else if (args[idx] == "--ua") {
+			if (opts.userAgent) {
+				throw new Error("User Agent already specified.");
+			}
+			idx += 1;
+			if (idx >= args.length || args[idx].length == 0) {
+				throw new Error("Missing argument for '--ua' parameter.");
+			}
+			opts.userAgent = args[idx];
+		}
+		else {
+			throw new Error("Unsupported parameter.");
+		}
+	}
+
+	if (!opts.privateKeyFile) {
+		throw new Error("Private key file not specified.");
+	}
+	if (!opts.applicationId) {
+		throw new Error("Application ID not specified.");
+	}
+	if (!opts.permissions) {
+		throw new Error("No access permissions has been specified.");
+	}
+	return opts;
+}
+
+function toBase64Url(buf) {
+	return buf.toString("base64").replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+}
+
+async function webRequest(url, options) {
+	return new Promise((resolve, reject) => {
+		const opts = {
+			method: (options && options.method) ? options.method : "GET",
+			timeout: (options && options.timeout) ? options.timeout : 10000
+		};
+		if (options && options.headers) {
+			opts.headers = options.headers;
+		}
+
+		const req = https.request(url, opts, (res) => {
+			const ret = {
+				statusCode: res.statusCode,
+				headers: res.headers,
+				data: ""
+			}
+
+			res.on("data", (chunk) => {
+				ret.data += chunk;
+			});
+
+			res.on("end", () => {
+				resolve(ret);
+			});
+		});
+
+		req.on("error", (err) => {
+			reject({ error: err });
+		});
+
+		if (options && options.body) {
+			req.write(options.body);
+		}
+		req.end();
+	});
+}
+
+async function readStdIn() {
+	return new Promise((resolve, reject) => {
+		let s = '';
+
+		process.stdin.setEncoding("utf8");
+		process.stdin.resume();
+		process.stdin.on('data', function (data) {
+			s += data;
+		});
+		process.stdin.on('end', function () {
+			process.stdin.pause();
+			resolve(s);
+		});
+	});
+}
